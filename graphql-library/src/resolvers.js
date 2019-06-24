f4664792400ae35104f3fd795fdd2d1f9b35ac2e
Phil Jung <seokhyun.phil.jung@gmail.com>
2019-06-24 11:05:03 +1000

    Made promise mapper a separate function for easier testing

--- graphql-library/src/resolvers.js
+++ graphql-library/src/resolvers.js
@@ -1,22 +1,19 @@
-function filterOutNonRecurringLists(lists) {
-  return lists.filter(list => list.name.toLowerCase().includes('recur'))
-}
-
 export default {
   ping: () => 'Living Trello of Takeshi: GraphQL API v0.0.1',
   board: ({id}, {Trello}) => 
     Trello.call(`/boards/${id}/lists`)
       .then(filterOutNonRecurringLists)
-      .then(lists => {
-        const promiseArr = lists.map(list => () => 
-          new Promise((resolve, reject) => 
-            Trello.call(`/lists/${list.id}/cards`)
-              .then(cards => resolve({...list, cards}))
-              .catch(err => reject('Failed Promise array: ' + err))
-          )
-        )
-        return Promise.all(promiseArr.map(execute => execute()))
-      })
+      .then(lists => Promise.all(lists.map(mapListToPromise(Trello))))
       .then(lists => ({lists}))
       .catch(console.error)
+}
+
+function filterOutNonRecurringLists(lists) {
+  return lists.filter(list => list.name.toLowerCase().includes('recur'))
+}
+
+function mapListToPromise(Trello) {
+  return list => Trello.call(`/lists/${list.id}/cards`)
+    .then(cards => ({...list, cards}))
+    .catch(err => console.error('Failed Promise array: ', err))
 }
